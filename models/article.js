15676a7621ac23d0d7c46e69e8afeaa9dbb4be15
Yoonha Kim <ivryxx@gmail.com>
2022-03-22 01:22:24 +0900

    add gitignore/ fix typeError

---
+++ models/article.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const articleSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  markdown: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: String,
+    // default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Article", articleSchema);
