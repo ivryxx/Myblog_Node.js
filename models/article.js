3b82e115168b51a7d992b2ce907f5e67a4b0fbf2
Yoonha Kim <ivryxx@gmail.com>
2022-03-22 15:33:11 +0900

    add markdown / dompurify

--- models/article.js
+++ models/article.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 const marked = require("marked");
 const slugify = require("slugify");
-// const createDomPurify = require("dompurify");
-// const { JSDOM } = require("jsdom");
-// const dompurify = createDomPurify(new JSDOM().window)
+const createDomPurify = require("dompurify");
+const { JSDOM } = require("jsdom");
+const dompurify = createDomPurify(new JSDOM().window);
 
 const articleSchema = new mongoose.Schema({
   title: {
@@ -26,11 +26,19 @@ const articleSchema = new mongoose.Schema({
     required: true,
     unique: true,
   },
+  sanitizedHtml: {
+    type: String,
+    required: true,
+  },
 });
 //slugify는 Url을 예쁘게 정리해줌
 articleSchema.pre("validate", function (next) {
   if (this.title) {
     this.slug = slugify(this.title, { lower: true, strict: true }); //lowercase로 변환,url 형식에 맞지 않는 것들 제거(:등등..)
+  }
+
+  if (this.markdown) {
+    this.sanitizedHtml = dompurify.sanitize(marked.parse(this.markdown));
   }
 
   next();
