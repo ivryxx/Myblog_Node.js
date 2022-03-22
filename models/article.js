7aecddffd18f6706b9e12b402d46fa201757410b
Yoonha Kim <ivryxx@gmail.com>
2022-03-22 14:28:18 +0900

    add slugify/ connect DB

--- models/article.js
+++ models/article.js
@@ -1,4 +1,9 @@
 const mongoose = require("mongoose");
+const marked = require("marked");
+const slugify = require("slugify");
+// const createDomPurify = require("dompurify");
+// const { JSDOM } = require("jsdom");
+// const dompurify = createDomPurify(new JSDOM().window)
 
 const articleSchema = new mongoose.Schema({
   title: {
@@ -13,9 +18,22 @@ const articleSchema = new mongoose.Schema({
     required: true,
   },
   createdAt: {
-    type: String,
-    // default: Date.now,
+    type: Date,
+    default: Date.now,
   },
+  slug: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+//slugify는 Url을 예쁘게 정리해줌
+articleSchema.pre("validate", function (next) {
+  if (this.title) {
+    this.slug = slugify(this.title, { lower: true, strict: true }); //lowercase로 변환,url 형식에 맞지 않는 것들 제거(:등등..)
+  }
+
+  next();
 });
 
 module.exports = mongoose.model("Article", articleSchema);
